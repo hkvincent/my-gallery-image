2341766014469769f6703ef2c5efde761a249bb4
vincent <hkvincentchan@gmail.com>
2024-03-09 01:11:47 +0800

    change sytle

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 const fs = require('fs');
 const sharp = require('sharp');
 
-
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
